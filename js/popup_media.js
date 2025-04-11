066a5f62221b53c864c017d7f41eb970ecb8cdb6
“philippe <philippe.mail.perso@gmail.com>
2025-04-11 10:44:13 +0200

    Update popup_media.js

--- js/popup_media.js
+++ js/popup_media.js
@@ -1,66 +1,26 @@
 'use strict';
-Object.assign(dotclear, dotclear.getData('admin.blog_pref'));
 dotclear.ready(() => {
-    const cancel = document.getElementById('media-insert-cancel');
-    if (cancel)
-        cancel.addEventListener('click', () => { window.close(); });
-
+    $('#media-select-cancel').on('click', () => {
+        window.close();
+    });
     if (window.opener) {
-        const button = document.querySelector('button[name="media-select-ok"]');
-
-        button.addEventListener('click', () => {
-
-            const main = window.opener;
-            const href = main.document.querySelector('input[name="url"]').value;
-            const thumburl = main.document.querySelectorAll('input[name="src"]')[2].value;
-            const buttonId = main.document.querySelector('input[name="change-button-id"]').value;
-
-            if (buttonId === 'default_image_selector') {
-                main.document.querySelector('#default_image_tb_url').value = thumburl;
-                const defaultImageUrl = main.document.querySelector('#default_image_url');
-                defaultImageUrl.value = href;
-                defaultImageUrl.dispatchEvent(new Event('change'));
-            }
-            if (buttonId === 'default_small_image_selector') {
-                main.document.querySelector('#default_small_image_tb_url').value = thumburl;
-                const defaultSmallImageUrl = main.document.querySelector('#default_small_image_url');
-                defaultSmallImageUrl.value = href;
-                defaultSmallImageUrl.dispatchEvent(new Event('change'));
-            }
-
-            window.close();
-        });
-
-    }
-});
-/*if (window.opener) {
-    const buttons = document.querySelectorAll('media-select-ok');
-    buttons.forEach(button => {
-        button.addEventListener('click', () => {
-
-
+        document.getElementById('media-select-ok').addEventListener('click', () => {
             const main = window.opener;
             const href = document.querySelector('input[name="url"]').value;
             const thumburl = document.querySelectorAll('input[name="src"]')[2].value;
             const buttonId = main.document.querySelector('input[name="change-button-id"]').value;
 
             if (buttonId === 'default_image_selector') {
-                main.document.querySelector('#default_image_tb_url').value = thumburl;
-                const defaultImageUrl = main.document.querySelector('#default_image_url');
-                defaultImageUrl.value = href;
-                defaultImageUrl.dispatchEvent(new Event('change'));
+                main.document.getElementById('default_image_tb_url').value = thumburl;
+                main.document.getElementById('default_image_url').value = href;
+                main.document.getElementById('default_image_url').dispatchEvent(new Event('change'));
             }
             if (buttonId === 'default_small_image_selector') {
-                main.document.querySelector('#default_small_image_tb_url').value = thumburl;
-                const defaultSmallImageUrl = main.document.querySelector('#default_small_image_url');
-                defaultSmallImageUrl.value = href;
-                defaultSmallImageUrl.dispatchEvent(new Event('change'));
+                main.document.getElementById('default_small_image_tb_url').value = thumburl;
+                main.document.getElementById('default_small_image_url').value = href;
+                main.document.getElementById('default_small_image_url').dispatchEvent(new Event('change'));
             }
             window.close();
-
         });
-    });
-}
-
- 
-}); */
+    }
+});
