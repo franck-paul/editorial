b30cf28d209495a77ef73c55595cbc52193bc765
“philippe <philippe.mail.perso@gmail.com>
2025-04-11 09:40:51 +0200

    Update popup_media.js

--- js/popup_media.js
+++ js/popup_media.js
@@ -1,22 +1,66 @@
 'use strict';
+Object.assign(dotclear, dotclear.getData('admin.blog_pref'));
 dotclear.ready(() => {
-    $('#media-select-cancel').on('click', () => {
-        window.close();
-    });
-    $('#media-select-ok').on('click', () => {
-        const main = window.opener;
-        const href = $('input[name="url"]').val();
-        const thumburl = $('input[name="src"]').eq(2).val();
-        let buttonId = main.$('input[name="change-button-id"]').val();
+    const cancel = document.getElementById('media-insert-cancel');
+    if (cancel)
+        cancel.addEventListener('click', () => { window.close(); });
+    const buttons = document.querySelectorAll('media-select-ok');
+    if (window.opener) {
+        for (const button of buttons) {
+            button.addEventListener('click', () => {
 
-        if (buttonId == 'default_image_selector') {
-            main.$('#default_image_tb_url').prop('value', thumburl);
-            main.$('#default_image_url').prop('value', href).trigger('change');
+                if (window.opener) {
+                    const main = window.opener;
+                    const href = document.querySelector('input[name="url"]').value;
+                    const thumburl = document.querySelectorAll('input[name="src"]')[2].value;
+                    const buttonId = main.document.querySelector('input[name="change-button-id"]').value;
+
+                    if (buttonId === 'default_image_selector') {
+                        main.document.querySelector('#default_image_tb_url').value = thumburl;
+                        const defaultImageUrl = main.document.querySelector('#default_image_url');
+                        defaultImageUrl.value = href;
+                        defaultImageUrl.dispatchEvent(new Event('change'));
+                    }
+                    if (buttonId === 'default_small_image_selector') {
+                        main.document.querySelector('#default_small_image_tb_url').value = thumburl;
+                        const defaultSmallImageUrl = main.document.querySelector('#default_small_image_url');
+                        defaultSmallImageUrl.value = href;
+                        defaultSmallImageUrl.dispatchEvent(new Event('change'));
+                    }
+                }
+                window.close();
+            });
         }
-        if (buttonId == 'default_small_image_selector') {
-            main.$('#default_small_image_tb_url').prop('value', thumburl);
-            main.$('#default_small_image_url').prop('value', href).trigger('change');
-        }
-        window.close();
-    });
+    }
 });
+/*if (window.opener) {
+    const buttons = document.querySelectorAll('media-select-ok');
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+
+
+            const main = window.opener;
+            const href = document.querySelector('input[name="url"]').value;
+            const thumburl = document.querySelectorAll('input[name="src"]')[2].value;
+            const buttonId = main.document.querySelector('input[name="change-button-id"]').value;
+
+            if (buttonId === 'default_image_selector') {
+                main.document.querySelector('#default_image_tb_url').value = thumburl;
+                const defaultImageUrl = main.document.querySelector('#default_image_url');
+                defaultImageUrl.value = href;
+                defaultImageUrl.dispatchEvent(new Event('change'));
+            }
+            if (buttonId === 'default_small_image_selector') {
+                main.document.querySelector('#default_small_image_tb_url').value = thumburl;
+                const defaultSmallImageUrl = main.document.querySelector('#default_small_image_url');
+                defaultSmallImageUrl.value = href;
+                defaultSmallImageUrl.dispatchEvent(new Event('change'));
+            }
+            window.close();
+
+        });
+    });
+}
+
+ 
+}); */
