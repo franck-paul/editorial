f82b709cf84c2cc407b4a786e55dc2673da3c13e
“philippe <philippe.mail.perso@gmail.com>
2025-04-11 10:57:08 +0200

    Update popup_media.js

--- js/popup_media.js
+++ js/popup_media.js
@@ -4,23 +4,25 @@ dotclear.ready(() => {
         window.close();
     });
     if (window.opener) {
-        document.getElementById('media-select-ok').addEventListener('click', () => {
-            const main = window.opener;
-            const href = document.querySelector('input[name="url"]').value;
-            const thumburl = document.querySelectorAll('input[name="src"]')[2].value;
-            const buttonId = main.document.querySelector('input[name="change-button-id"]').value;
+        if (document.getElementById('media-select-ok')) {
+            document.getElementById('media-select-ok').addEventListener('click', () => {
+                const main = window.opener;
+                const href = document.querySelector('input[name="url"]').value;
+                const thumburl = document.querySelectorAll('input[name="src"]')[2].value;
+                const buttonId = main.document.querySelector('input[name="change-button-id"]').value;
 
-            if (buttonId === 'default_image_selector') {
-                main.document.getElementById('default_image_tb_url').value = thumburl;
-                main.document.getElementById('default_image_url').value = href;
-                main.document.getElementById('default_image_url').dispatchEvent(new Event('change'));
-            }
-            if (buttonId === 'default_small_image_selector') {
-                main.document.getElementById('default_small_image_tb_url').value = thumburl;
-                main.document.getElementById('default_small_image_url').value = href;
-                main.document.getElementById('default_small_image_url').dispatchEvent(new Event('change'));
-            }
-            window.close();
-        });
+                if (buttonId === 'default_image_selector') {
+                    main.document.getElementById('default_image_tb_url').value = thumburl;
+                    main.document.getElementById('default_image_url').value = href;
+                    main.document.getElementById('default_image_url').dispatchEvent(new Event('change'));
+                }
+                if (buttonId === 'default_small_image_selector') {
+                    main.document.getElementById('default_small_image_tb_url').value = thumburl;
+                    main.document.getElementById('default_small_image_url').value = href;
+                    main.document.getElementById('default_small_image_url').dispatchEvent(new Event('change'));
+                }
+                window.close();
+            });
+        }
     }
 });
