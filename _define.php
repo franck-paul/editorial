46c33944d5985a07328e22fd64787f1a876e3ddf
“philippe <philippe.mail.perso@gmail.com>
2025-05-07 19:14:08 +0200

    fix select dark

--- _define.php
+++ _define.php
@@ -12,7 +12,7 @@ $this->registerModule(
     'Éditorial',
     'A theme for Dotclear',
     'Philippe aka amalgame and contributors',
-    '7.1',
+    '7.2',
     [
         'requires'          => [['core', '2.33']],
         'standalone_config' => true,
