50d1d9ea989f7e23d0d470985fae55a0af4fd7d6
“philippe <philippe.mail.perso@gmail.com>
2025-05-08 15:32:41 +0200

    new version

--- _define.php
+++ _define.php
@@ -12,7 +12,7 @@ $this->registerModule(
     'Éditorial',
     'A theme for Dotclear',
     'Philippe aka amalgame and contributors',
-    '7.3',
+    '7.4',
     [
         'requires'          => [['core', '2.33']],
         'standalone_config' => true,
