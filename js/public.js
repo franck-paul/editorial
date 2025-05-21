10fcf7ab815070452fcc9b3c17ee838bd77b3a1f
“philippe <philippe.mail.perso@gmail.com>
2025-05-21 17:54:05 +0200

    fix toggle and sidebar display

--- js/public.js
+++ js/public.js
@@ -3,22 +3,25 @@ document.addEventListener('DOMContentLoaded', () => {
     const sidebar = document.getElementById("sidebar");
 
     sidebar.classList.add("open");
+    toggle.classList.add("open");
     
     toggle.addEventListener("click", () => {
         if (sidebar.classList.contains("open")) {
             sidebar.classList.remove("open");
             sidebar.classList.add("close");
+            toggle.classList.remove("open");
+            toggle.classList.add("close");
             console.log("sidebar closed");
         } else {
             sidebar.classList.remove("close");
             sidebar.classList.add("open");
+            toggle.classList.remove("close");
+            toggle.classList.add("open");
             console.log("sidebar opened");
         }
 
     });
 
-
-    // totop scroll
     $(window).scroll(function () {
         if ($(this).scrollTop() != 0) {
             $('#gotop').fadeIn();
