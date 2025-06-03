36986c76a790fb9daf47132090541f4c76cab8d0
“philippe <philippe.mail.perso@gmail.com>
2025-06-03 16:53:22 +0200

    Update _define.php

--- _define.php
+++ _define.php
@@ -12,7 +12,7 @@ $this->registerModule(
     'Éditorial',
     'A theme for Dotclear',
     'Philippe aka amalgame and contributors',
-    '9.3',
+    '9.4',
     [
         'date'              => '2025-06-02T00:00:13+0100',
         'requires'          => [['core', '2.33']],
